21ef55f00a4107b2058695614f81043671417590
JOHN MARK GACUSANA <gacusanajohnmark1986@gmail.com>
2025-02-17 22:24:04 +0800

    assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+/* CS1A_JMGacusana_C1-A1.js
+C1-A1 Variable/Constants Declaration and String Concatenation Methods.
+I use let to declare the variable name and double quotation for strings.
+*/
+
+let name = "JUAN GAMOSO DELA CRUZ";
+let birthdate = "January 1, 2001";
+let birth_place = "UPPER BONIFACIO, BAGUIO CITY, BENGUET, PHILIPPINES 2600";
+let address = "SAN NICOLAS, CANDON CITY, ILOCOS SUR, PHILIPPINES 2710";
+let CourseAndYear = "bachelor of science in computer science";
+let dream_job_after_graduation = "Software Engineer";
+
+/* Number 2. printing the information of the students.
+I use console.log for printing the declared variables
+*/
+console.log(name, "was born", birthdate, "at", birth_place, "and currently living at", address, name.toLowerCase(), /*i use the command "toLowerCase" to make the upper case into lower case inside the name variable*/ "is taking up", CourseAndYear, "and dreams to be", dream_job_after_graduation, "after graduation.");
