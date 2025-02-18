1e9f2773b16b153e9c9b9fb81197d992a5944509
JOHN MARK GACUSANA <gacusanajohnmark1986@gmail.com>
2025-02-18 10:00:34 +0800

    assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,16 +1,17 @@
 /* CS1A_JMGacusana_C1-A1.js
 C1-A1 Variable/Constants Declaration and String Concatenation Methods.
 I use let to declare the variable name and double quotation for strings.
+
 */
 
-let name = "JUAN GAMOSO DELA CRUZ";
-let birthdate = "January 1, 2001";
-let birth_place = "UPPER BONIFACIO, BAGUIO CITY, BENGUET, PHILIPPINES 2600";
-let address = "SAN NICOLAS, CANDON CITY, ILOCOS SUR, PHILIPPINES 2710";
-let CourseAndYear = "bachelor of science in computer science";
-let dream_job_after_graduation = "Software Engineer";
+let name1 = "JUAN GAMOSO DELA CRUZ";
+let birthdate1 = "January 1, 2001";
+let birth_place1 = "UPPER BONIFACIO, BAGUIO CITY, BENGUET, PHILIPPINES 2600";
+let address1 = "SAN NICOLAS, CANDON CITY, ILOCOS SUR, PHILIPPINES 2710";
+let CourseAndYear1 = "bachelor of science in computer science";
+let dream_job_after_graduation1 = "Software Engineer";
 
 /* Number 2. printing the information of the students.
 I use console.log for printing the declared variables
 */
-console.log(name, "was born", birthdate, "at", birth_place, "and currently living at", address, name.toLowerCase(), /*i use the command "toLowerCase" to make the upper case into lower case inside the name variable*/ "is taking up", CourseAndYear, "and dreams to be", dream_job_after_graduation, "after graduation.");
+console.log(name1, "was born", birthdate1, "at", birth_place, "and currently living at", address1, name1.toLowerCase(), /*i use the command "toLowerCase" to make the upper case into lower case inside the name variable*/ "is taking up", CourseAndYear1, "and dreams to be", dream_job_after_graduation, "after graduation.");
